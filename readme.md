8cdbf44d748ba186f080b010c445b9107badc993
justin <justinphil02@yahoo.com>
2021-08-03 13:13:12 -0700

    add basic content to server.py. create templates folder. move index.html to templates

---
+++ readme.md
@@ -0,0 +1,13 @@
+Goals:
+
+This site will serve two purposes
+1. A homepage / about description of the project - an autonomous home garden with data collection 
+2. a data collection api - the rasberry pi will send data to the server intemittently 
+3. view aggregate data
+
+
+To Do:
+
+Set up POST route for API
+Create a view page for API data
+Set up database
---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request, render_template
+
+
+# Creating a new "app" by using the Flask constructor. Passes __name__ as a parameter.
+app = Flask(__name__)
+
+
+#Routes 
+
+@app.route("/")
+def index():
+	return render_template('index.html')
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	print("POST request")
+	return "Hello World!"
+
+
+
+
+# Checks to see if the name of the package is the run as the main package.
+if __name__ == "__main__":
+	# Runs the Flask application only if the main.py file is being run.
+	app.run()
